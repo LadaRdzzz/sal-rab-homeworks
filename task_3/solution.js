a41f443b955bbeb5c40b1c8d788f07e2a9e6fea3
ladar <lada.rodzina@gmail.com>
2022-08-15 21:25:00 +0300

    task 3 is done

--- task_3/solution.js
+++ task_3/solution.js
@@ -23,6 +23,17 @@ function renderCartItem(item) {
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
+    //let itemCountText = "";
+    //itemCountText = itemCountText + product.count;
+    //itemCountText = itemCountText + ` × `;
+    //itemCountText = itemCountText + product.price;
+    //itemCountText = itemCountText + ` ₽ = `;
+    //let sum = product.count * product.price;
+    //itemCountText = itemCountText + sum + ` ₽`; 
+
+    const sum = product.count * product.price;
+    const itemCountText = `${product.count} × ${product.price} ₽ = ${sum} ₽`;
+
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
